12c23544df50f588f330d976a00578ca1d7158e8
F4tall <kamil.boksa@gmail.com>
2020-10-25 17:23:39 +0100

    Add repos

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -3,7 +3,4 @@ package guru.springframework.sfgpetclinic.services;
 import guru.springframework.sfgpetclinic.model.Owner;
 
 public interface OwnerService extends CrudService<Owner, Long> {
-
-    Owner findByLastName(Long name);
-
 }
